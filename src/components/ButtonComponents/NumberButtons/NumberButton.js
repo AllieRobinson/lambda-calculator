7c862f00e6fb4a9cf6fc8dee065e42b91b74e1ee
Allie Robinson <allierob93@gmail.com>
2019-08-27 13:58:33 -0500

    Numberbutton components rendering successfully.

--- src/components/ButtonComponents/NumberButtons/NumberButton.js
+++ src/components/ButtonComponents/NumberButtons/NumberButton.js
@@ -1,11 +1,11 @@
 import React from "react";
 
-const NumberButton = () => {
+const NumberButton = (props) => {
   return (
     <button className="number-button">
-      {/* Display a button element rendering the data being passed down from the parent container on props */}
+      {props.text}
     </button>
   );
 };
 
-export default NumberButton
+export default NumberButton;
