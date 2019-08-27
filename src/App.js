e615960226ec50322cb34d1b3f6b3c9c30b339ed
Allie Robinson <allierob93@gmail.com>
2019-08-27 18:09:51 -0500

    Styling begun.

--- src/App.js
+++ src/App.js
@@ -26,9 +26,9 @@ function App() {
       <div className="App">
         {/* STEP 4 - Render your components here and be sure to properly import/export all files */}
         <Display number={displayValue} />
-        <Numbers addNumber={addNumber} />
         <Operators />
         <Specials />
+        <Numbers addNumber={addNumber} />
       </div>
     </div>
   );
--- src/components/DisplayComponents/Display.js
+++ src/components/DisplayComponents/Display.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Display = (props) => {
-  return <div>
+  return <div className="display">
             {props.number}
          </div>;
 };
