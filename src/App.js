872175686b7fee647ec2b1ab315c3c7ecaf766fa
Allie Robinson <allierob93@gmail.com>
2019-08-27 14:36:14 -0500

    Operator buttons now rendering.

--- src/App.js
+++ src/App.js
@@ -6,7 +6,8 @@ import "./App.css";
 // Logo has already been provided for you. Do the same for the remaining components
 import Logo from "./components/DisplayComponents/Logo";
 import Display from './components/DisplayComponents/Display.js';
-import Numbers from './components/ButtonComponents/NumberButtons/Numbers.js'
+import Numbers from './components/ButtonComponents/NumberButtons/Numbers.js';
+import Operators from './components/ButtonComponents/OperatorButtons/Operators.js';
 
 function App() {
   // STEP 5 - After you get the components displaying using the provided data file, write your state hooks here.
@@ -20,8 +21,9 @@ function App() {
       <Logo />
       <div className="App">
         {/* STEP 4 - Render your components here and be sure to properly import/export all files */}
-        <Display number={76} />
+        <Display />
         <Numbers />
+        <Operators />
       </div>
     </div>
   );
